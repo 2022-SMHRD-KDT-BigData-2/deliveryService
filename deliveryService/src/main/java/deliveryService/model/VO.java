47d9de2b5150ba1d64cc57e1bdfca72d84bada82
smhrd <smhrd@220.71.167.6>
2022-02-11 12:15:06 +0900

    VO 생성자 추가

--- deliveryService/src/main/java/deliveryService/model/VO.java
+++ deliveryService/src/main/java/deliveryService/model/VO.java
@@ -3,4 +3,11 @@ package deliveryService.model;
 public class VO {
 	private String name;
 	private int age;
+	
+	public VO(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	
 }
