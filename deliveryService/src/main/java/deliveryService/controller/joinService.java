15c4ad173f3af3769f49447518bee2375226ca77
hckio <hckio@125.136.103.233>
2022-02-23 18:18:14 +0900

    Maven 업데이트 mybatis 환경 구축 joinservice 생성

---
+++ deliveryService/src/main/java/deliveryService/controller/joinService.java
@@ -0,0 +1,15 @@
+package deliveryService.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class joinService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
--- deliveryService/src/main/java/deliveryService/model/DAO.java
+++ deliveryService/src/main/java/deliveryService/model/DAO.java
@@ -1,5 +1,24 @@
 package deliveryService.model;
 
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 public class DAO {
+	private static SqlSessionFactory sqlSessionFactory;{
+		
+	}
+	static {
+		try {
+			String resource = "Mapper/config.xml";
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			
+		} catch (Exception e) {
+			
+		}
+    }
 
 }
